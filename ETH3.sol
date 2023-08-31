8fb0294896c9095156f9c8c152f214a25febc81d
diya26322632 <138868720+diya26322632@users.noreply.github.com>
2023-08-31 14:07:11 +0530

    Update ETH3.sol

--- ETH3.sol
+++ ETH3.sol
@@ -12,7 +12,7 @@ interface IERC20 {
     event Approval(address indexed owner, address indexed spender, uint amount);
 }
 
-contract ERC20 is IERC20 {
+contract DAToken is IERC20 {
     address public immutable owner;
     uint public totalSupply;
     mapping(address => uint) public balanceOf;
@@ -59,13 +59,13 @@ contract ERC20 is IERC20 {
         return true;
     }
     
-    function mint(uint amount) external onlyOwner {
+    function mintDAToken(uint amount) external onlyOwner {
         balanceOf[msg.sender] += amount;
         totalSupply += amount;
         emit Transfer(address(0), msg.sender, amount);
     }
     
-    function burn(uint amount) external {
+    function burnDAToken(uint amount) external {
         require(amount > 0, "Amount should not be zero");
         require(balanceOf[msg.sender] >= amount, "Insufficient Balance");
         
@@ -73,5 +73,37 @@ contract ERC20 is IERC20 {
         totalSupply -= amount;
 
         emit Transfer(msg.sender, address(0), amount);
+    }
+
+    function getOwnerInfo() external view returns (address ownerAddress, uint ownerBalance) {
+        ownerAddress = owner;
+        ownerBalance = balanceOf[owner];
+    }
+
+    function getTransactionDetail(address sender, address recipient) external pure returns (string memory detail) {
+        detail = string(abi.encodePacked("From: ", toString(sender), "\nTo: ", toString(recipient), "\nAmount: "));
+    }
+
+    function toString(address account) internal pure returns (string memory) {
+        return toAsciiString(uint256(uint160(account)));
+    }
+
+    function toAsciiString(uint256 value) internal pure returns (string memory) {
+        if (value == 0) {
+            return "0";
+        }
+        uint256 temp = value;
+        uint256 digits;
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+        bytes memory buffer = new bytes(digits);
+        while (value != 0) {
+            digits -= 1;
+            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+            value /= 10;
+        }
+        return string(buffer);
     }
 }
